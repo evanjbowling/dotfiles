91842d06169682cb056bcc334b2a35ee16160cb2
Evan Bowling <evan.j.bowling@gmail.com>
2017-04-19 00:51:26 -0500

    Update install.sh

--- osx/install.sh
+++ osx/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install homebrew
+# (check site for latest installation script)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install yarn
 brew install yarn
