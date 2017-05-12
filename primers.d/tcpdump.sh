2700d5658079d8f288f71302afa835083db6704f
Evan Bowling <evan.j.bowling@gmail.com>
2017-05-11 22:22:15 -0500

    Update tcpdump.sh

--- primers.d/tcpdump.sh
+++ primers.d/tcpdump.sh
@@ -4,5 +4,11 @@
 # 1.list interfaces
 sudo tcpdump -D
 
-# 2. listen to a particular interface
-sudo tcpdump -i 8
+# 2. list to all interfaces and be less verbose (quiet flag)
+sudo tcpdump -i any -q
+
+# 3. listen to a particular interface
+sudo tcpdump -i 8 #/en0/eth0
+
+# 4. listen to all tcp traffic
+sudo tcpdump -i any -q tcp
