c2d50f6ceeb616990bd05116b4f098f27f0b7200
Evan Bowling <evan.j.bowling@gmail.com>
2017-05-25 17:51:18 -0500

    Update docker.sh

--- primers.d/docker.sh
+++ primers.d/docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# 1. stop & remove all exited containers
+# 1. run simple container
+docker run --rm -it python:2 /bin/bash
+
+# 2. stop & remove all exited containers
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a q)
