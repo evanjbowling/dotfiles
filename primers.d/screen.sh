18ef267068e192210defb24b72ebf0abe12d5b62
Evan Bowling <evan.j.bowling@gmail.com>
2017-08-05 16:19:19 -0500

    Create screen.sh

---
+++ primers.d/screen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# 1. start screen session
+screen
+
+# 2. view screen help menu while within session
+# Ctrl-a,?
+
+# 3. detach from screen session
+# Ctrl-a,d
+
+# 4. re-attach to screen session
+screen -r
