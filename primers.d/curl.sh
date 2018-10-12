5df8d89a68e8088c954ed3eccb4075f22bb132ee
Evan Bowling <evan.j.bowling@gmail.com>
2018-10-11 23:05:59 -0500

    Update curl.sh with more curl examples
    - GET/POST examples

--- primers.d/curl.sh
+++ primers.d/curl.sh
@@ -6,4 +6,8 @@ curl http://localhost:42/
 # 2. curl IPv6 localhost request
 curl -g -6 --noproxy "*" "http://[::1]:42/"
 
+# 3. GET request with increased verbosity
+curl -vvv -X GET http://server:1234
 
+# 4. POST request with application/json content
+curl -X POST -H "Content-Type: application/json" http://server:1234 -d '{"key":[1,2,"val"]}'
