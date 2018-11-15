457e0a53c545ae6741132cf73ace36e2552cf004
Evan Bowling <evan.j.bowling@gmail.com>
2018-11-14 23:11:37 -0600

    Update docker.sh

--- primers.d/docker.sh
+++ primers.d/docker.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+# 0. check what containers are running
+docker ps -a
+
 # 1. run simple container
 docker run --rm -it python:2 /bin/bash
 
 # 2. stop & remove all exited containers
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a q)
+
+# 3. remove container
+docker rm <CONTAINER_ID>
+
+# 4. remove image
+docker rmi <IMAGE_ID>
