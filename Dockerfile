73124a5edb85e9aa7f23f3cb176901c1b9e3bc84
Evan Bowling <evan.j.bowling@gmail.com>
2021-05-31 16:45:35 -0500

    Update Dockerfile and entry-point.sh

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,6 @@
-FROM ubuntu:16.04
-
-RUN apt-get -y update
-RUN apt-get -y upgrade
-RUN apt-get -y install vim \
-                       curl \
-                       git \
-                       openjdk-8-jdk                      
-
-ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
-
+FROM ubuntu:18.04
+RUN apt-get -y update && \
+    apt-get -y install vim curl git
 COPY src/docker/entry-point.sh /entry-point.sh
-
 ENTRYPOINT ["/entry-point.sh"]
+
--- src/docker/entry-point.sh
+++ src/docker/entry-point.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 # Purpose: provide a single place to trigger different types of
 #          tests (er. as root, as non-root user)
-curl https://raw.githubusercontent.com/evanjbowling/dotfiles/master/installer > installer && chmod +x installer && ./installer && rm ./installer && bash
+mkdir ~/.ejb
+git clone https://github.com/evanjbowling/dotfiles.git ~/.ejb/dotfiles
+
+echo 'if [ -f "$HOME/.ejb/dotfiles/.bash_ejb" ]; then' >> ~/.bashrc
+echo '  . "$HOME/.ejb/dotfiles/.bash_ejb"' >> ~/.bashrc
+echo 'fi' >> ~/.bashrc
+
+bash
+
